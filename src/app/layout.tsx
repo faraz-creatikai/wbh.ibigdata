0034a51fdaef627d3397efacd5b313bfec408f10
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-03-02 14:59:15 +0530

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -174,7 +174,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                                 </div>
 
                                 {/* Actual Page */}
-                                <div className=" bg-white max-md:px-2 max-md:py-2 max-sm:mt-[52px]">{children}</div>
+                                <div className="max-md:px-2 max-md:py-2 max-sm:mt-[52px]">{children}</div>
                               </main>
                             </SidebarInset>
                           </div>
