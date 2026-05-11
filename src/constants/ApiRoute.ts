f2338c87ad5c0e1aaedc534ae7187f392556de27
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-05-11 12:41:44 +0530

    u

--- src/constants/ApiRoute.ts
+++ src/constants/ApiRoute.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://apiwbh.ibigdata.in/api";
 //https://live-project-backend-1.onrender.com
 //https://propertyapi.aileadgenie.cloud/api
 //https://apidomain.ibigdata.in
@@ -438,4 +438,4 @@ export const API_ROUTES = {
   }
 };
 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://apiwbh.ibigdata.in";
