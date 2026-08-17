abeeb2882dd1e493344464ab4d49767d14c74857
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-17 11:25:47 +0530

    U

--- src/constants/ApiRoute.ts
+++ src/constants/ApiRoute.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://apitravel.ibigdata.in/api";
+export const BASE_URL = "https://apiedu.ibigdata.in/api";
 //https://live-project-backend-1.onrender.com
 //https://propertyapi.aileadgenie.cloud/api
 //https://apidomain.ibigdata.in
@@ -503,4 +503,4 @@ export const API_ROUTES = {
   }
 };
 
-export const API_URL = "https://apitravel.ibigdata.in";
+export const API_URL = "https://apiedu.ibigdata.in";
